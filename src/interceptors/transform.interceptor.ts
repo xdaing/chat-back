fd982c6fa2d906b4337ff5f26de9ba3a0843455b
咩咩 <2380848997@qq.com>
2023-02-22 23:50:25 +0800

    处理相应格式

---
+++ src/interceptors/transform.interceptor.ts
@@ -0,0 +1,9 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common'
+import { map } from 'rxjs/operators'
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler) {
+        return next.handle().pipe(map(data => ({ statusCode: 200, data })))
+    }
+}
