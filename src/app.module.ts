f12c1ad49e70271e6557db6ae701a953dbf05224
咩咩 <2380848997@qq.com>
2023-02-22 23:51:24 +0800

    配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common'
+import { APP_GUARD } from '@nestjs/core'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
+import { SocketModule } from '@/socket/socket.module'
 import { UserModule } from '@/user/user.module'
+import { JwtModule } from '@/jwt/jwt.module'
+import { FileModule } from '@/file/file.module'
+import { StaticModule } from '@/static/static.module'
 
 @Module({
-  imports: [UserModule]
+  imports: [SocketModule, UserModule, ThrottlerModule.forRoot({ ttl: 60, limit: 100 }), JwtModule, FileModule, StaticModule],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }]
 })
 
 export class AppModule { }
